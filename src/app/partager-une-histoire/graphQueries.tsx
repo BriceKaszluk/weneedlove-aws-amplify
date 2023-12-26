f2f490a943d65e7fadf73c5cce315f5c13c358d7
BriceKaszluk <kaszlukb@gmail.com>
2023-12-26 17:38:18 +0100

    feat(shareStory): restricitions crud on stories + manage sending story

--- src/app/partager-une-histoire/graphQueries.tsx
+++ src/app/partager-une-histoire/graphQueries.tsx
@@ -8,11 +8,28 @@ export const addStory = async (title: string, content: string) => {
     title,
     content
   };
-  
-  const newStory = await client.graphql({
-    query: mutations.createStory,
-    variables: { input: storyDetails }
-  });
 
-  console.log(newStory)
-}
+  try {
+    const response = await client.graphql({
+      query: mutations.createStory,
+      variables: { input: storyDetails }
+    });
+
+    if (response.data && (!response.errors || response.errors.length === 0)) {
+      console.log("Success:", response.data);
+      return "success";
+    } else if (response.errors) {
+      console.error("Error:", response.errors);
+      return `error: ${response.errors.map(error => error.message).join(', ')}`;
+    } else {
+      return "error: Erreur inconnue";
+    }
+  } catch (error) {
+    console.error("Error lors de l'exécution de la requête:", error);
+    if (error instanceof Error) {
+      return `error: ${error.message}`;
+    }
+    return "error: Erreur de requête inconnue";
+  }
+};
+
--- src/app/partager-une-histoire/page.tsx
+++ src/app/partager-une-histoire/page.tsx
@@ -1,14 +1,64 @@
 "use client";
-import { useState } from 'react';
-import { TextField, TextAreaField, Text, Button } from "@aws-amplify/ui-react";
+import { useState } from "react";
+import {
+  TextField,
+  TextAreaField,
+  Text,
+  Button,
+  Alert,
+} from "@aws-amplify/ui-react";
 import { addStory } from "./graphQueries";
 
 import AuthenticatedLayout from "../layouts/AuthenticatedLayout";
 
 export default function ShareStory() {
-
   const [storyTitle, setStoryTitle] = useState("");
   const [storyContent, setStoryContent] = useState("");
+  const [shareSuccess, setShareSuccess] = useState<boolean | string>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  let message;
+  if (shareSuccess === "success") {
+    message = (
+      <Alert
+        variation="success"
+        isDismissible={true}
+        hasIcon={true}
+        heading="Félicitations!"
+        onDismiss={() => {
+          setShareSuccess("");
+        }}
+      >
+        Ton histoire a bien été partagée
+      </Alert>
+    );
+  } else if (
+    typeof shareSuccess === "string" &&
+    shareSuccess.startsWith("error:")
+  ) {
+    message = (
+      <Alert
+        variation="success"
+        isDismissible={true}
+        hasIcon={true}
+        heading="Erreur"
+        onDismiss={() => {
+          setShareSuccess("");
+        }}
+      >
+        {shareSuccess.substring(6)}
+      </Alert>
+    );
+  }
+
+  const shareStory = async () => {
+    setIsLoading(true);
+    const result = await addStory(storyTitle, storyContent);
+    setShareSuccess(result);
+    setStoryTitle("");
+    setStoryContent("");
+    setIsLoading(false)
+  };
 
   return (
     <AuthenticatedLayout>
@@ -18,24 +68,33 @@ export default function ShareStory() {
         placeholder="Baggins"
         label="Last name"
         errorMessage="There is an error"
-        onChange={(e) => {setStoryTitle(e.target.value)}}
+        isDisabled = {isLoading ? true : false}
+        onChange={(e) => {
+          setStoryTitle(e.target.value);
+        }}
       />
       <TextAreaField
         descriptiveText="Rédiges ton histoire ici"
         label=""
         name="last_name"
         placeholder="Baggins"
+        isDisabled = {isLoading ? true : false}
         rows={3}
-        onChange={(e) => {setStoryContent(e.target.value)}}
+        onChange={(e) => {
+          setStoryContent(e.target.value);
+        }}
       />
+      {message}
       <Button
         variation="primary"
         colorTheme="success"
+        isDisabled = {isLoading ? true : false}
         loadingText=""
-        onClick={() => addStory(storyTitle, storyContent)}
+        onClick={() => shareStory()}
       >
         Partager
       </Button>
+      
     </AuthenticatedLayout>
   );
 }
